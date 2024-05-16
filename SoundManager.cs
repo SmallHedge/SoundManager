78d951960d1ff0224f6bbf15fa2708ebed2c35c4
Ryan <95608156+SmallHedge@users.noreply.github.com>
2024-05-16 16:59:52 +1200

    Update SoundManager.cs
    Updated SoundManager.cs to work with the new Editor Script

--- SoundManager.cs
+++ SoundManager.cs
@@ -1,53 +1,60 @@
 //Author: Small Hedge Games
-//Published: 12/04/2024
+//Updated: 16/05/2024
 
 using UnityEngine;
 using System;
 
-public enum SoundType
+namespace SHG.SoundManager
 {
-    //ADD SOUND EFFECT NAMES
-}
-
-[RequireComponent(typeof(AudioSource)), ExecuteInEditMode]
-public class SoundManager : MonoBehaviour
-{
-    [SerializeField] private SoundList[] soundList;
-    private static SoundManager instance;
-    private AudioSource audioSource;
-
-    private void Awake()
+    public enum SoundType
     {
-        instance = this;
+        //ADD SOUND EFFECT NAMES
     }
 
-    private void Start()
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundManager : MonoBehaviour
     {
-        audioSource = GetComponent<AudioSource>();
+        [SerializeField] private SoundList[] soundList;
+        private static SoundManager instance;
+        private AudioSource audioSource;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+                Destroy(gameObject);
+        }
+
+        private void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        public static void PlaySound(SoundType sound, float volume = 1)
+        {
+            AudioClip[] clips = instance.soundList[(int)sound].Sounds;
+            AudioClip randomClip = clips[UnityEngine.Random.Range(0, clips.Length)];
+            instance.audioSource.PlayOneShot(randomClip, volume);
+        }
+
+        public void Resize()
+        {
+            string[] names = Enum.GetNames(typeof(SoundType));
+            Array.Resize(ref soundList, names.Length);
+            for (int i = 0; i < soundList.Length; i++)
+                soundList[i].name = names[i];
+        }
     }
 
-    public static void PlaySound(SoundType sound, float volume = 1)
+    [Serializable]
+    public struct SoundList
     {
-        AudioClip[] clips = instance.soundList[(int)sound].Sounds;
-        AudioClip randomClip = clips[UnityEngine.Random.Range(0, clips.Length)];
-        instance.audioSource.PlayOneShot(randomClip, volume);
+        public AudioClip[] Sounds { get => sounds; }
+        [HideInInspector] public string name;
+        [SerializeField] private AudioClip[] sounds;
     }
-
-#if UNITY_EDITOR
-    private void OnEnable()
-    {
-        string[] names = Enum.GetNames(typeof(SoundType));
-        Array.Resize(ref soundList, names.Length);
-        for (int i = 0; i < soundList.Length; i++)
-            soundList[i].name = names[i];
-    }
-#endif
-}
-
-[Serializable]
-public struct SoundList
-{
-    public AudioClip[] Sounds { get => sounds; }
-    [HideInInspector] public string name;
-    [SerializeField] private AudioClip[] sounds;
 }
