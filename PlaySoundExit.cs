3c37c9d6cf61db9590278a83066def75657649d0
Ryan <95608156+SmallHedge@users.noreply.github.com>
2024-05-16 17:01:01 +1200

    Update PlaySoundExit.cs
    Updated namespace

--- PlaySoundExit.cs
+++ PlaySoundExit.cs
@@ -3,12 +3,15 @@
 
 using UnityEngine;
 
-public class PlaySoundExit : StateMachineBehaviour
+namespace SHG.SoundManager
 {
-    [SerializeField] private SoundType sound;
-    [SerializeField, Range(0, 1)] private float volume = 1;
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public class PlaySoundExit : StateMachineBehaviour
     {
-        SoundManager.PlaySound(sound, volume);
+        [SerializeField] private SoundType sound;
+        [SerializeField, Range(0, 1)] private float volume = 1;
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            SoundManager.PlaySound(sound, volume);
+        }
     }
 }
