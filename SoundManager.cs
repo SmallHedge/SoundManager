2c05441feb27224056659e2891aaba89dcaa3586
Ryan <95608156+SmallHedge@users.noreply.github.com>
2024-05-21 13:58:17 +1200

    Update SoundManager.cs
    Added Audio Mixers and Sound scale. Resize() now only reshuffles if the size of the list has changed

--- SoundManager.cs
+++ SoundManager.cs
@@ -1,20 +1,16 @@
 //Author: Small Hedge Games
-//Updated: 20/05/2024
+//Updated: 21/05/2024
 
 using UnityEngine;
 using System;
 using System.Collections.Generic;
+using UnityEngine.Audio;
 
 namespace SHG.SoundManager
 {
     public enum SoundType
     {
-        TEST,
-        UI_HOVER,
-        UI_CLICK,
-        UI_GAMESTART,
-        UI_OPTIONS,
-        UI_QUIT,
+        //Add Sound Names Here
     }
 
     [RequireComponent(typeof(AudioSource))]
@@ -27,39 +23,48 @@ namespace SHG.SoundManager
         private void Awake()
         {
             instance = this;
-        }
-
-        private void Start()
-        {
             audioSource = GetComponent<AudioSource>();
         }
 
         public static void PlaySound(SoundType sound, float volume = 1)
         {
-            AudioClip[] clips = instance.soundList[(int)sound].sounds;
+            SoundList soundList = instance.soundList[(int)sound];
+            AudioClip[] clips = soundList.sounds;
             AudioClip randomClip = clips[UnityEngine.Random.Range(0, clips.Length)];
-            instance.audioSource.PlayOneShot(randomClip, volume);
+            instance.audioSource.outputAudioMixerGroup = soundList.mixer;
+            instance.audioSource.PlayOneShot(randomClip, volume * soundList.volume);
         }
 
         public void Resize()
         {
-            Dictionary<string, AudioClip[]> clips = new();
-            for (int i = 0; i < soundList.Length; ++i)
-            {
-                if (soundList[i].sounds.Length > 0)
-                    clips.Add(soundList[i].name, soundList[i].sounds);
-            }
-
             string[] names = Enum.GetNames(typeof(SoundType));
+            bool differentSize = names.Length != soundList.Length;
+
+            Dictionary<string, AudioClip[]> clips = new();
+
+            if(differentSize)
+            {
+                for (int i = 0; i < soundList.Length; ++i)
+                {
+                    if (soundList[i].sounds != null)
+                        clips.Add(soundList[i].name, soundList[i].sounds);
+                }
+            }
+            
             Array.Resize(ref soundList, names.Length);
             for (int i = 0; i < soundList.Length; i++)
             {
                 string currentName = names[i];
                 soundList[i].name = currentName;
-                if (clips.ContainsKey(currentName))
-                    soundList[i].sounds = clips[currentName];
-                else
-                    soundList[i].sounds = null;
+                if (soundList[i].volume == 0) soundList[i].volume = 1;
+
+                if (differentSize)
+                {
+                    if (clips.ContainsKey(currentName))
+                        soundList[i].sounds = clips[currentName];
+                    else
+                        soundList[i].sounds = null;
+                }
             }
         }
     }
@@ -68,6 +73,8 @@ namespace SHG.SoundManager
     public struct SoundList
     {
         [HideInInspector] public string name;
+        [Range(0, 1)] public float volume;
+        public AudioMixerGroup mixer;
         public AudioClip[] sounds;
     }
 }
