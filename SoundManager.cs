4f058ea3dbb7d260f54af5221c21732874f56259
Ryan <95608156+SmallHedge@users.noreply.github.com>
2024-06-13 16:04:14 +1200

    Update SoundManager.cs
    Updated to have audio mixers and a volume slider

--- SoundManager.cs
+++ SoundManager.cs
@@ -1,70 +1,45 @@
 //Author: Small Hedge Games
-//Updated: 21/05/2024
+//Updated: 13/06/2024
 
-using UnityEngine;
 using System;
-using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Audio;
 
 namespace SHG.SoundManager
 {
-    public enum SoundType
-    {
-        //Add Sound Names Here
-    }
-
     [RequireComponent(typeof(AudioSource))]
     public class SoundManager : MonoBehaviour
     {
-        [SerializeField] private SoundList[] soundList;
-        private static SoundManager instance;
+        [SerializeField] private SoundsSO SO;
+        private static SoundManager instance = null;
         private AudioSource audioSource;
 
         private void Awake()
         {
-            instance = this;
-            audioSource = GetComponent<AudioSource>();
+            if(!instance)
+            {
+                instance = this;
+                audioSource = GetComponent<AudioSource>();
+            }
         }
 
-        public static void PlaySound(SoundType sound, float volume = 1)
+        public static void PlaySound(SoundType sound, AudioSource source = null, float volume = 1)
         {
-            SoundList soundList = instance.soundList[(int)sound];
+            SoundList soundList = instance.SO.sounds[(int)sound];
             AudioClip[] clips = soundList.sounds;
             AudioClip randomClip = clips[UnityEngine.Random.Range(0, clips.Length)];
-            instance.audioSource.outputAudioMixerGroup = soundList.mixer;
-            instance.audioSource.PlayOneShot(randomClip, volume * soundList.volume);
-        }
 
-        public void Resize()
-        {
-            string[] names = Enum.GetNames(typeof(SoundType));
-            bool differentSize = names.Length != soundList.Length;
-
-            Dictionary<string, AudioClip[]> clips = new();
-
-            if(differentSize)
+            if(source)
             {
-                for (int i = 0; i < soundList.Length; ++i)
-                {
-                    if (soundList[i].sounds != null)
-                        clips.Add(soundList[i].name, soundList[i].sounds);
-                }
+                source.outputAudioMixerGroup = soundList.mixer;
+                source.clip = randomClip;
+                source.volume = volume * soundList.volume;
+                source.Play();
             }
-            
-            Array.Resize(ref soundList, names.Length);
-            for (int i = 0; i < soundList.Length; i++)
+            else
             {
-                string currentName = names[i];
-                soundList[i].name = currentName;
-                if (soundList[i].volume == 0) soundList[i].volume = 1;
-
-                if (differentSize)
-                {
-                    if (clips.ContainsKey(currentName))
-                        soundList[i].sounds = clips[currentName];
-                    else
-                        soundList[i].sounds = null;
-                }
+                instance.audioSource.outputAudioMixerGroup = soundList.mixer;
+                instance.audioSource.PlayOneShot(randomClip, volume * soundList.volume);
             }
         }
     }
